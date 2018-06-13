74bc0aadf055c13a06ae91462a79249b17189af2
Zhilin Cai <zhilin.cai@icloud.com>
2018-06-13 16:25:58 +0100

    wrote ORM, Model and a test

---
+++ www/test.py
@@ -0,0 +1,15 @@
+import orm, asyncio
+from models import User, Blog, Comment
+
+async def test():
+    await orm.create_pool(loop=loop, user='sb', password='sb', db='awesome')
+
+    u = User(name='Test5', email='test7@example.com', passwd='1234567890', image='about:blank')
+
+    await u.save()
+
+# 获取EventLoop:
+loop = asyncio.get_event_loop()
+
+#把协程丢到EventLoop中执行
+loop.run_until_complete(test())
